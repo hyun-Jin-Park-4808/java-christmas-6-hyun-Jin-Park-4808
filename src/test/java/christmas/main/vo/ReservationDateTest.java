3990c9a2d8b83a0ed96081353df36e61cb1228fc
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-10 15:48:55 +0900

    test: ReservationDate 생성자에 1~31 범위의 숫자가 들어오면 예외를 발생시키지 않는지 테스트

--- src/test/java/christmas/main/vo/ReservationDateTest.java
+++ src/test/java/christmas/main/vo/ReservationDateTest.java
@@ -1,10 +1,8 @@
 package christmas.main.vo;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,9 +11,17 @@ class ReservationDateTest {
     @DisplayName("1~31 범위 외의 숫자가 들어오면 예외가 발생한다.")
     @ValueSource(strings = {"-1", "0", "32"})
     @ParameterizedTest
-    void createReservationDate(Integer input) {
+    void createReservationDateByOutOfRange(Integer input) {
         assertThatThrownBy(() -> new ReservationDate(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("1~31 범위의 숫자가 들어오면 예외를 발생시키지 않는다.")
+    @ValueSource(strings = {"1", "5", "31"})
+    @ParameterizedTest
+    void createReservationDate(Integer input) {
+        new ReservationDate(input);
+    }
+
 
 }
