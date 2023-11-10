d8b3f82c963faf5189802cd512662b64dc5b5802
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-10 14:09:49 +0900

    feat: readLine()을 사용한 입력 기능 구현
    - 방문 날짜, 그리고 주문 메뉴와 개수를 입력받기 위해 구현합니다.

---
+++ src/main/java/christmas/ui/InputService.java
@@ -0,0 +1,13 @@
+package christmas.ui;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputService {
+
+    public static String userInput() {
+        return readLine();
+    }
+
+
+
+}
