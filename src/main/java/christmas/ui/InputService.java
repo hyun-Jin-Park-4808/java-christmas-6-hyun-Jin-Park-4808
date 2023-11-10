4d49f6d990ba2eaa606b1cd58a07958631e6139c
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-10 21:47:29 +0900

    feat: 구분자 "-"와 ","로 구분해서 배열로 변환하는 기능 구현
    - 내부 기능으로 정규식을 이용해 입력 양식이 맞는지 검증하며, 메뉴의 개수가 1 이상 20 이하의 숫자가 맞는지에 대한 검증도 포함합니다.
    
    - 메뉴판에 없는 메뉴를 주문하지 않았는지 검증하는 기능도 내부 기능으로 포함됩니다.

--- src/main/java/christmas/ui/InputService.java
+++ src/main/java/christmas/ui/InputService.java
@@ -1,12 +1,13 @@
 package christmas.ui;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static christmas.menus.service.MenuService.getAllMenus;
 
 import christmas.main.vo.ReservationDate;
 import christmas.ui.error.ErrorMessage;
+import java.util.regex.Pattern;
 
 public class InputService {
-
 
     public static String userInput() {
         return readLine();
@@ -23,8 +24,39 @@ public class InputService {
         } catch (Exception e) {
             throw new IllegalArgumentException(ErrorMessage.INCORRECT_FORMAT_OF_DATE_EXCEPTION);
         }
+    }
 
+    public static String[] splitUserInputForOrder(String userInputForOrder) {
+        validateFormatOfInputForOrder(userInputForOrder);
+        String[] menus = userInputForOrder.split(",|-");
+        validateExistenceOfOrderMenu(menus, getAllMenus());
+        return menus;
+    }
+
+    private static void validateExistenceOfOrderMenu(String[] menus, String allMenus) {
+        for (int i = 0; i < menus.length / 2; i++) {
+            validateInclusion(isIncluded(menus, allMenus, i));
+        }
+    }
+
+    private static void validateInclusion(boolean isIncluded) {
+        if (!isIncluded) {
+            throw new IllegalArgumentException(ErrorMessage.INCORRECT_FORMAT_OF_ORDER_EXCEPTION);
+        }
+    }
+
+    private static boolean isIncluded(String[] menus, String allMenus, int i) {
+        return allMenus.contains(menus[i * 2]);
     }
 
 
+
+    private static void validateFormatOfInputForOrder(String userInputForOrder) {
+        String formatOfOrder = "^[가-힣]+-[1-20]$";
+        for (String menu : userInputForOrder.split(",")) {
+            if (!Pattern.matches(formatOfOrder, menu)) {
+                throw new IllegalArgumentException(ErrorMessage.INCORRECT_FORMAT_OF_ORDER_EXCEPTION);
+            }
+        }
+    }
 }
