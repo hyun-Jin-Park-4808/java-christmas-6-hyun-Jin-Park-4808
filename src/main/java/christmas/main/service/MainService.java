03db3e4e062d93fe8acd580a4400c8692b1ced7d
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-13 15:43:30 +0900

    feat: 크리스마스 디데이 할인 이벤트 금액 책정 기능 구현
    - 내부 기능으로 다음과 같은 기능이 포함됩니다.
    - 크리스마스 디데이 할인 이벤트 기간인지 확인하는 기능
    - 할인액 계산을 위해 1일로부터 며칠이 지났는지 계산하는 기능
    - 각 할인액을 계산하는 기능들은 내부 기능보다는 핵심 기능 자체이므로 기능 목록의 내부 기능에서 삭제합니다.

--- src/main/java/christmas/main/service/MainService.java
+++ src/main/java/christmas/main/service/MainService.java
@@ -6,6 +6,9 @@ import java.util.Map;
 public class MainService {
 
     public static final int MIN_PRICE_FOR_EVENT = 10000;
+    public static final int LAST_DATE_OF_CHRISTMAS_EVENT = 25;
+    public static final int MIN_DISCOUNT_AMOUNT_OF_CHRISTMAS_EVENT = 1000;
+    public static final int ADDED_DISTCOUNT_AMOUNT_OF_CHRISTMAS_EVENT = 100;
 
     public static int calculateTotalPriceBeforeDiscount(Map<Menu, Integer> orders) {
         int totalPrice = 0;
@@ -22,5 +25,25 @@ public class MainService {
             return true;
         }
         return false;
+    }
+
+    public static int calculateDiscountAmountOfChristmasEvent(int date) {
+        if (isNotDuringTheEventPeriod(date)) {
+            return 0;
+        }
+        return MIN_DISCOUNT_AMOUNT_OF_CHRISTMAS_EVENT
+                + calculateAddedDiscountAmountOfChristmasEvent(date);
+    }
+
+    private static int calculateAddedDiscountAmountOfChristmasEvent(int date) {
+        return calculatePassedDaysSinceThe1st(date) * ADDED_DISTCOUNT_AMOUNT_OF_CHRISTMAS_EVENT;
+    }
+
+    private static int calculatePassedDaysSinceThe1st(int date) {
+        return date - 1;
+    }
+
+    private static boolean isNotDuringTheEventPeriod(int date) {
+        return date > LAST_DATE_OF_CHRISTMAS_EVENT;
     }
 }
