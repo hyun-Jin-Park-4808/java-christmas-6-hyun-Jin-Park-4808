3bcfcc8aab4f13b7e2eda6127a0d871df9af29a0
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-13 17:04:02 +0900

    test: 평일(일요일~목요일) 할인 금액 책정 기능 테스트
    - 주말을 입력할 경우 0원을 반환하는 테스트와 평일을 입력할 경우 디저트의 개수를 세서 할인 금액을 반환하는 테스트를 진행합니다.

--- src/test/java/christmas/main/service/MainServiceTest.java
+++ src/test/java/christmas/main/service/MainServiceTest.java
@@ -1,6 +1,7 @@
 package christmas.main.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 import christmas.menus.type.Menu;
 import java.util.HashMap;
@@ -13,21 +14,29 @@ class MainServiceTest {
     public static final int NUMBER_OF_TAPAS = 2;
     public static final int NUMBER_OF_CHOCOLATE_CAKE = 2;
     public static final int NUMBER_OF_T_BONE_STEAK = 1;
+    public static final int NUMBER_OF_RED_WINE = 5;
     public static final int MAX_DISCOUNT_AMOUNT_OF_CHRISTMAS_EVENT = 3400;
 
-    @DisplayName("할인 전 총 주문 금액을 계산한다.")
-    @Test
-    void calculateTotalPriceBeforeDiscount() {
-        // given
+    private static Map<Menu, Integer> testOrders() {
         Map<Menu, Integer> input = new HashMap<>();
 
         input.put(Menu.TAPAS, NUMBER_OF_TAPAS);
         input.put(Menu.CHOCOLATE_CAKE, NUMBER_OF_CHOCOLATE_CAKE);
         input.put(Menu.T_BONE_STEAK, NUMBER_OF_T_BONE_STEAK);
+        input.put(Menu.RED_WINE, NUMBER_OF_RED_WINE);
+        return input;
+    }
+
+    @DisplayName("할인 전 총 주문 금액을 계산한다.")
+    @Test
+    void calculateTotalPriceBeforeDiscount() {
+        // given
+        Map<Menu, Integer> input = testOrders();
 
         int expectedResult = Menu.TAPAS.getPrice() * NUMBER_OF_TAPAS
                 + Menu.CHOCOLATE_CAKE.getPrice() * NUMBER_OF_CHOCOLATE_CAKE
-                + Menu.T_BONE_STEAK.getPrice() * NUMBER_OF_T_BONE_STEAK;
+                + Menu.T_BONE_STEAK.getPrice() * NUMBER_OF_T_BONE_STEAK
+                + Menu.RED_WINE.getPrice() * NUMBER_OF_RED_WINE;
 
         // when
         int result = MainService.calculateTotalPriceBeforeDiscount(input);
@@ -35,6 +44,8 @@ class MainServiceTest {
         // then
         assertThat(result).isEqualTo(expectedResult);
     }
+
+
 
     @DisplayName("25일 이후의 날짜를 입력하면 크리스마스 할인 금액은 0원이다.")
     @Test
@@ -60,6 +71,37 @@ class MainServiceTest {
 
         // then
         assertThat(result).isEqualTo(MAX_DISCOUNT_AMOUNT_OF_CHRISTMAS_EVENT);
+    }
+
+    @DisplayName("입력한 일자가 평일이 아니면 평일 할인 금액이 0원으로 반환된다.")
+    @Test
+    void calculateDiscountAmountOfWeekdayEventByWeekend() {
+        // given
+        int date = 8; // 목요일
+        Map<Menu, Integer> input = testOrders();
+
+        // when
+        int result = MainService.calculateDiscountAmountOfWeekdayEvent(date, input);
+
+        // then
+        assertThat(result).isEqualTo(0);
+    }
+
+    @DisplayName("입력한 일자가 평일이면 평일 할인 금액이 적용된다.")
+    @Test
+    void calculateDiscountAmountOfWeekdayEvent() {
+        // given
+        int date = 7; // 목요일
+        Map<Menu, Integer> input = testOrders();
+
+        int expectedResult
+                = MainService.DISCOUNT_AMOUNT_PER_MENU * NUMBER_OF_CHOCOLATE_CAKE;
+
+        // when
+        int result = MainService.calculateDiscountAmountOfWeekdayEvent(date, input);
+
+        // then
+        assertThat(result).isEqualTo(expectedResult);
     }
 
 
