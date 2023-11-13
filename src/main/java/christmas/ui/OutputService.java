986c86df31b739878c2689ed1d045e06285886bc
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-13 20:35:26 +0900

    feat: 이벤트 플래너 소개 문구 출력 기능 구현

--- src/main/java/christmas/ui/OutputService.java
+++ src/main/java/christmas/ui/OutputService.java
@@ -1,4 +1,10 @@
 package christmas.ui;
 
 public class OutputService {
+    public static void printIntroductionText() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+
+
 }
