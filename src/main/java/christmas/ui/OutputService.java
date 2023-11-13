b82d4a20deefddb8e7971c81e46bb726364ddb39
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-13 20:46:45 +0900

    feat: 식당 예상방문 날짜 입력 문구 출력 기능 구현

--- src/main/java/christmas/ui/OutputService.java
+++ src/main/java/christmas/ui/OutputService.java
@@ -5,6 +5,9 @@ public class OutputService {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
 
+    public static void printTextForReservationDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
 
 
 
