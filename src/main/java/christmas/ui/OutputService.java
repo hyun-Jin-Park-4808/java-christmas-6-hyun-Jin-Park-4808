3198a074b9ca23208c0139aa5dd14474db90f739
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-13 20:50:58 +0900

    feat: 혜택 미리 보기 문구 출력 기능 구현

--- src/main/java/christmas/ui/OutputService.java
+++ src/main/java/christmas/ui/OutputService.java
@@ -13,5 +13,10 @@ public class OutputService {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
     }
 
+    public static void printTextForEventContents(int date) {
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n", date);
+    }
+
+
 
 }
