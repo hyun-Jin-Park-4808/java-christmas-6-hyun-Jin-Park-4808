24e90df0e60d04888998833d3a45daddeb145af3
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2023-11-13 17:03:04 +0900

    feat: 평일(일요일~목요일) 할인 금액 책정 기능 구현
    - 내부 기능으로 평일인지 확인하는 기능과, 디저트 메뉴를 세는 기능이 포함됩니다.

--- src/main/java/christmas/main/service/MainService.java
+++ src/main/java/christmas/main/service/MainService.java
@@ -1,7 +1,11 @@
 package christmas.main.service;
 
 import christmas.menus.type.Menu;
+import christmas.menus.type.MenuType;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class MainService {
 
@@ -9,10 +13,15 @@ public class MainService {
     public static final int LAST_DATE_OF_CHRISTMAS_EVENT = 25;
     public static final int MIN_DISCOUNT_AMOUNT_OF_CHRISTMAS_EVENT = 1000;
     public static final int ADDED_DISTCOUNT_AMOUNT_OF_CHRISTMAS_EVENT = 100;
+    public static final int YEAR_FOR_EVENT = 2023;
+    public static final int MONTH_FOR_EVNET = 12;
+    public static final int FRIDATY_NUMBER = 5;
+    public static final int SATURDAY_NUMBER = 6;
+    public static final int DISCOUNT_AMOUNT_PER_MENU = 2023;
 
     public static int calculateTotalPriceBeforeDiscount(Map<Menu, Integer> orders) {
         int totalPrice = 0;
-        for (Map.Entry<Menu, Integer> order : orders.entrySet()) {
+        for (Entry<Menu, Integer> order : orders.entrySet()) {
             int priceOfOrdersPerMenu = order.getKey().getPrice();
             int numberOfOrdersPerMenu = order.getValue();
             totalPrice += priceOfOrdersPerMenu * numberOfOrdersPerMenu;
@@ -46,4 +55,33 @@ public class MainService {
     private static boolean isNotDuringTheEventPeriod(int date) {
         return date > LAST_DATE_OF_CHRISTMAS_EVENT;
     }
+
+    public static int calculateDiscountAmountOfWeekdayEvent(int date, Map<Menu, Integer> orders) {
+        if (!isWeekday(date)) {
+            return 0;
+        }
+        return getNumberOfDesserts(orders) * DISCOUNT_AMOUNT_PER_MENU;
+    }
+
+    private static int getNumberOfDesserts(Map<Menu, Integer> orders) {
+        return orders.entrySet().stream()
+                .filter(entry -> entry.getKey().isSameMenuType(MenuType.DESSERT))
+                .mapToInt(entry -> entry.getValue())
+                .sum();
+    }
+
+    private static boolean isWeekday(int date) {
+        LocalDate localDate = LocalDate.of(YEAR_FOR_EVENT, MONTH_FOR_EVNET, date);
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        int dayOfWeekNumber = dayOfWeek.getValue();
+        if (isNotWeekday(dayOfWeekNumber)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isNotWeekday(int dayOfWeekNumber) {
+        return dayOfWeekNumber >= FRIDATY_NUMBER && dayOfWeekNumber <= SATURDAY_NUMBER;
+    }
+
 }
